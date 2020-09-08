37641e9e43e7cee06b275f3d689f88c316d86b13
victorscholz <vgscholz@gmail.com>
2020-09-08 00:18:28 -0400

    credit cards rendered under user

--- src/index.js
+++ src/index.js
@@ -1,46 +1,81 @@
 document.addEventListener("DOMContentLoaded", () => {
-  //   const Fetcheroo = new FetchAdapter("https://localhost:3000/");
-  //   const getUsersCallback = (users) => users.forEach(console.log * "users");
-  //   const getCreditCardsCallback = (creditCards) =>
-  //     creditCards.forEach(console.log * "ccs");
-  //   Fetcheroo.get("users", getUsersCallback);
-  //   Fetcheroo.get("credit_cards", getCreditCardsCallback);
-});
+    // const Fetcheroo = new FetchAdapter("http://localhost:3000/");
+    // const getUsersCallback = (users) => users.forEach(console.log * "users");
+    // const getCreditCardsCallback = (creditCards) =>
+    //   creditCards.forEach(console.log * "ccs");
+    // Fetcheroo.get("users", getUsersCallback);
+    // Fetcheroo.get("credit_cards", getCreditCardsCallback);
+
+const fetchCards = () => {
+  fetch("http://localhost:3000/credit_cards/")
+  .then(response => response.json())
+  .then(cards => renderCards(cards));
+}
+
+const renderCards = (cards) => {
+  for(const aCard of cards) {
+    renderCard(aCard)
+  }
+}
+
+function renderCard(aCard) {
+  // const cardContainer = document.querySelector("#credit-card-list")
+  // const cardDiv = document.querySelector("#cards")
+  // cardDiv.classList.add("credit-cards")
+  
+  // cardDiv.innerHTML = `
+  // <h4>${aCard.name}</h4>
+  // <h4>$${aCard.annual_fee}</h4>
+  // `
+  // cardContainer.append(cardDiv)
+  // console.log(aCard)
+  
+    const cardDiv = document.querySelector('div#cards')
+    const newCardDiv = document.createElement('div')
+    newCardDiv.innerHTML = `
+    <h4> ${aCard.name} | $${aCard.annual_fee} </h4>
+    `
+    cardDiv.append(newCardDiv)
+  
+}
 
 // submit listener for user information
 // post to my information list
-fetch("https://localhost:3000/credit_cards")
-  .then((response) => response.json())
-  .then((creditCards) => creditCards.forEach(console.log));
+const submitHandler = () => {
+  document.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const pointsForm = document.querySelector("#create-points-form");
+    const cashForm = document.querySelector("#create-cash-form");
+    const expirationForm = document.querySelector("#create-expirations-form");
+    const informationDiv = document.querySelector("#information");
 
-document.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const pointsForm = document.querySelector("#create-points-form");
-  const cashForm = document.querySelector("#create-cash-form");
-  const expirationForm = document.querySelector("#create-expirations-form");
-  if (e.target === pointsForm) {
-    const pointInput = document.querySelector("#accrued-points").value;
-    const pointLi = document.createElement("li");
-    pointLi.textContent = pointInput;
+    if (e.target === pointsForm) {
+      const pointInput = document.querySelector("#accrued-points").value;
+      const pointDiv = document.createElement("div");
+      pointDiv.textContent = pointInput;
 
-    const informationUl = document.querySelector("#information");
-    informationUl.append(pointLi);
-    pointsForm.reset();
-  } else if (e.target === cashForm) {
-    const cashInput = document.querySelector("#accrued-cash").value;
-    const cashLi = document.createElement("li");
-    cashLi.textContent = cashInput;
+      informationDiv.append(pointDiv);
+      pointsForm.reset();
 
-    const cashUl = document.querySelector("#information");
-    cashUl.append(cashLi);
-    cashForm.reset();
-  } else if (e.target === expirationForm) {
-    const expirationInput = document.querySelector("#point-expirations").value;
-    const expirationLi = document.createElement("li");
-    expirationLi.textContent = expirationInput;
+    } else if (e.target === cashForm) {
+      const cashInput = document.querySelector("#accrued-cash").value;
+      const cashDiv = document.createElement("div");
+      cashDiv.textContent = cashInput;
 
-    const informationUl = document.querySelector("#information");
-    informationUl.append(expirationLi);
-    expirationForm.reset();
-  }
+      informationDiv.append(cashDiv);
+      cashForm.reset();
+
+    } else if (e.target === expirationForm) {
+      const expirationInput = document.querySelector("#point-expirations").value;
+      const expirationDiv = document.createElement("div");
+      expirationDiv.textContent = expirationInput;
+
+      informationDiv.append(expirationDiv);
+      expirationForm.reset();
+    }
+  });
+}
+
+fetchCards()
+submitHandler()
 });
