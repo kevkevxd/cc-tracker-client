fa549489de86607a97222ef434f4b10720e9c5ab
victorscholz <vgscholz@gmail.com>
2020-09-07 21:26:00 -0400

    seed data now available on index.html

--- src/index.js
+++ src/index.js
@@ -1,46 +1,47 @@
 document.addEventListener("DOMContentLoaded", () =>{
 
-    const Fetcheroo = new FetchAdapter("https://localhost:3000/");
+    // const Fetcheroo = new FetchAdapter("https://localhost:3000/");
 
     const getUsersCallback = users => users.forEach(console.log*("users"))
     const getCreditCardsCallback = creditCards => creditCards.forEach(console.log*("ccs"))
 
-    Fetcheroo.get("users", getUsersCallback)
-    Fetcheroo.get("creditcards", getCreditCardsCallback)
+    // Fetcheroo.get("users", getUsersCallback)
+    // Fetcheroo.get("credit_cards", getCreditCardsCallback)
 
 })
 
 // submit listener for user information
 // post to my information list
+fetch("http://localhost:3000/credit_cards")
+.then((response) => response.json())
+.then((creditCards) => creditCards.forEach(console.log))
 
 document.addEventListener("submit", e => {
     e.preventDefault()
     const pointsForm = document.querySelector("#create-points-form")
     const cashForm = document.querySelector("#create-cash-form")
     const expirationForm = document.querySelector("#create-expirations-form")
+    const informationDiv = document.querySelector("#information")
     if(e.target === pointsForm){
         const pointInput = document.querySelector("#accrued-points").value
-        const pointLi = document.createElement("li")
-        pointLi.textContent = pointInput
+        const pointDiv = document.createElement("div")
+        pointDiv.textContent = pointInput
 
-        const informationUl = document.querySelector("#information")
-        informationUl.append(pointLi)
+        informationDiv.append(pointDiv)
         pointsForm.reset()
     } else if(e.target === cashForm){
         const cashInput = document.querySelector("#accrued-cash").value
-        const cashLi = document.createElement("li")
-        cashLi.textContent = cashInput
+        const cashDiv = document.createElement("div")
+        cashDiv.textContent = cashInput
 
-        const cashUl = document.querySelector("#information")
-        cashUl.append(cashLi)
+        informationDiv.append(cashDiv)
         cashForm.reset()
     } else if(e.target === expirationForm){
         const expirationInput = document.querySelector("#point-expirations").value
-        const expirationLi = document.createElement("li")
-        expirationLi.textContent = expirationInput
+        const expirationDiv = document.createElement("div")
+        expirationDiv.textContent = expirationInput
 
-        const informationUl = document.querySelector("#information")
-        informationUl.append(expirationLi)
+        informationDiv.append(expirationDiv)
         expirationForm.reset()
     }   
 
