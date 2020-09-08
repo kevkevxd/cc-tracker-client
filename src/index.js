5746071966921263caa2284eee8a875f6e55bd86
victorscholz <vgscholz@gmail.com>
2020-09-07 20:35:29 -0400

    working on user forms

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ document.addEventListener("DOMContentLoaded", () =>{
 document.addEventListener("submit", e => {
     e.preventDefault()
     const pointsForm = document.querySelector("#create-points-form")
+    const cashForm = document.querySelector("#create-cash-form")
+    const expirationForm = document.querySelector("#create-expirations-form")
     if(e.target === pointsForm){
         const pointInput = document.querySelector("#accrued-points").value
         const pointLi = document.createElement("li")
@@ -23,7 +25,23 @@ document.addEventListener("submit", e => {
 
         const informationUl = document.querySelector("#information")
         informationUl.append(pointLi)
-        form.reset()
-    }
+        pointsForm.reset()
+    } else if(e.target === cashForm){
+        const cashInput = document.querySelector("#accrued-cash").value
+        const cashLi = document.createElement("li")
+        cashLi.textContent = cashInput
+
+        const cashUl = document.querySelector("#information")
+        cashUl.append(cashLi)
+        cashForm.reset()
+    } else if(e.target === expirationForm){
+        const expirationInput = document.querySelector("#point-expirations").value
+        const expirationLi = document.createElement("li")
+        expirationLi.textContent = expirationInput
+
+        const informationUl = document.querySelector("#information")
+        informationUl.append(expirationLi)
+        expirationForm.reset()
+    }   
 
 })
