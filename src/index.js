245bf02282dd93c3e83d85dc1ac10982465afb2a
kevin_xie <xie.kevin.j@gmail.com>
2020-09-07 21:23:23 -0400

    css

--- src/index.js
+++ src/index.js
@@ -1,29 +1,30 @@
-document.addEventListener("DOMContentLoaded", () =>{
+document.addEventListener("DOMContentLoaded", () => {
+  const Fetcheroo = new FetchAdapter("https://localhost:3000/");
 
-    const Fetcheroo = new FetchAdapter("https://localhost:3000/");
+  //   const getUsersCallback = (users) => users.forEach(console.log * "users");
+  //   const getCreditCardsCallback = (creditCards) =>
+  //     creditCards.forEach(console.log * "ccs");
 
-    const getUsersCallback = users => users.forEach(console.log*("users"))
-    const getCreditCardsCallback = creditCards => creditCards.forEach(console.log*("ccs"))
-
-    Fetcheroo.get("users", getUsersCallback)
-    Fetcheroo.get("creditcards", getCreditCardsCallback)
-
-})
+  //   Fetcheroo.get("users", getUsersCallback);
+  //   Fetcheroo.get("credit_cards", getCreditCardsCallback);
+});
 
 // submit listener for user information
 // post to my information list
+fetch("https://localhost:3000/credit_cards")
+  .then((response) => response.json())
+  .then((creditCards) => creditCards.forEach(console.log));
 
-document.addEventListener("submit", e => {
-    e.preventDefault()
-    const pointsForm = document.querySelector("#create-points-form")
-    if(e.target === pointsForm){
-        const pointInput = document.querySelector("#accrued-points").value
-        const pointLi = document.createElement("li")
-        pointLi.textContent = pointInput
+document.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const pointsForm = document.querySelector("#create-points-form");
+  if (e.target === pointsForm) {
+    const pointInput = document.querySelector("#accrued-points").value;
+    const pointLi = document.createElement("li");
+    pointLi.textContent = pointInput;
 
-        const informationUl = document.querySelector("#information")
-        informationUl.append(pointLi)
-        form.reset()
-    }
-
-})
+    const informationUl = document.querySelector("#information");
+    informationUl.append(pointLi);
+    form.reset();
+  }
+});
