7d50ed2f0ed163e752f1f7dbe42aab1299f27031
victorscholz <vgscholz@gmail.com>
2020-09-10 00:18:09 -0400

    editing my-cards and bookmarking cards

--- src/index.js
+++ src/index.js
@@ -34,20 +34,26 @@ document.addEventListener("DOMContentLoaded", () => {
         )
     }    
           const renderCard = (myCard) => {
-            let browseHeader = ce('div')
-            // console.log(myCard.name)
-            browseHeader.innerHTML =`
-            <h3><div>CC: ${myCard.name}</div>
+            let creditCard = ce('div')
+            creditCard.innerHTML =`
+            <div>CC: ${myCard.name}</div>
             <div>approval_date: ${myCard.approval_date}</div>
-            <div> bonus amount:${myCard.bonus_amount}</div></h3>
-            <div><input type="radio" name="list" value="delete-card">delete-card <button>delete</button></div>
-            <div><button class="edit-self-card">Default</button></div>
+            <div> bonus amount:${myCard.bonus_amount}</div>          
+            <div><button class="edit-self-card">delete</button></div>
+
+            <form id="edit-card-date-form" action="#" method="post">
+            <input type="number" id="date-form" name="${myCard.id}" placeholder="approval date">
+            <input type="submit" id="approval-date-edit" name="${myCard.id}" value="enter">
+            
+            <form id="enter-bonus-form" action="#" method="post"> 
+            <input type="number" id="bonus-amt" name="${myCard.id}" placeholder="bonus amount">
+            <input type="submit" id="acquired-bonus" name="${myCard.id}" value="enter">
+            
             `
-            // // credit card id associated with user
-            churnBody.append(browseHeader)
+            churnBody.append(creditCard)
           }
 
-    const updateCard = (id, formdata) => {
+    const updateCard = (ccId, formData) => {
       let configObj = {
       method: "PATCH",
       headers: {
@@ -56,8 +62,12 @@ document.addEventListener("DOMContentLoaded", () => {
       },
       body: JSON.stringify(formData)
     }
-    fetch(`http://localhost:3000/users/${id}`, configObj)
-    .then(jsonRes)}
+    fetch(`http://localhost:3000/credit_cards/${ccId}`, configObj)
+      .then(jsonRes)
+      .then(data => console.log(data))
+  } 
+
+  
 //-----------------All cards-------------------
     const fetchBrowse = () => {
       fetch(creditCardUrl)
@@ -73,29 +83,57 @@ document.addEventListener("DOMContentLoaded", () => {
     const renderBrowseCard = (aCard) => {
       const cardDiv = qs("div#cards");
       const newCardDiv = ce("div");
+      newCardDiv.dataset.num = aCard.id
+      
+      newCardDiv.dataset.bookMark = aCard.is_bookedmarked
+      if(newCardDiv.dataset.bookMark != null){
+        newCardDiv.dataset.bookMark = false
+      }
       newCardDiv.innerHTML = `
       <h4> ${aCard.name} | Fee: $${aCard.annual_fee} </h4>
-      <button>Bookmark</button>
+      <button id="bookmark-button">Bookmark</button>
       <h6> ${aCard.earn_description} </h6>
       `;
       churnBody.append(newCardDiv);
     }
 // ---------------------------Bookmarked Cards----------------------------
 
-  const getBookmarks = () => {
-    fetch(userUrl)
-    .then(jsonRes)
-    .then(users => {for (const user of users){
-      renderBookmark(user)
-    }})
-  }
-  const renderBookmark = (user) => {
-    let bookmarkButtonDiv = ce('div')
-    bookmarkButtonDiv.dataset.num = user.id
-    
+  const updateBookmark = (ccId, dataObject) => {
+    let configObj = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(dataObject)
+    }
+    fetch(`http://localhost:3000/credit_cards/${ccId}`, configObj)
+      .then(jsonRes)
+      .then(data => console.log(data))
+
   }
   
-
+  // const getBookmarks = () => {
+  //   fetch(userUrl)
+  //   .then(jsonRes)
+  //   .then(users => renderBookmarks(users))
+  // }
+  
+  // const renderBookmarks = (users) => {
+  //   for (const card of users) {
+  //     renderBookmark(card)
+  //   }
+  // }
+  // const renderBookmark = (user) => {
+  // let bookmarkButtonDiv = ce('div')
+  //   bookmarkButtonDiv.dataset.num = user.id
+  //   bookmarkButtonDiv.dataset.bookMark = user.is_bookedmarked
+  //   console.log(bookmarkButtonDiv)
+  //   // correctly associates id and is_bookmarked
+  //   churnBody.append(bookmarkButtonDiv)
+  // }
+  
+  // getBookmarks()
 // ----------------------User stats on right bar---------------------------
   const getUsers = () => {
     fetch(userUrl)
@@ -109,18 +147,41 @@ document.addEventListener("DOMContentLoaded", () => {
     currentUserStats.classList.add("user-stats")
     currentUserStats.setAttribute("data-id", `${user.id}`) 
     currentUserStats.setAttribute("data-cash", `${user.accrued_cash}`)
+    if(currentUserStats.dataset.cash === "null"){
+      currentUserStats.dataset.cash = 0
+    }
     currentUserStats.setAttribute("data-points", `${user.accrued_points}`)
-    currentUserStats.innerHTML = `
-    Points: ${user.accrued_points} | Cash: $${user.accrued_cash}
-    `
-    rightSideBar.append(currentUserStats)    
-  }
+    if(currentUserStats.dataset.points === "null"){
+      currentUserStats.dataset.points = 0
+    }
+    // let dc1 = dc.dataset.cash
+    rightSideBar.append(currentUserStats)  
+    let dc = qs(".user-stats")
+    let dc1 = dc.dataset.cash
+    let dp = dc.dataset.points 
 
+    // currentUserStats.innerHTML = `
+    // Points: ${user.accrued_points} | Cash: $${user.accrued_cash}
+    // `
+    let displayDiv = ce('div')
+    displayDiv.innerHTML = `Points: ${dp}  Cash: ${dc1}`
+    currentUserStats.append(displayDiv)
+  }
+  
   const submitHandler = () => {
+    // add code to reset form 
+    // form.reset()
+    // const pointsForm = qs("#accrued-points")
+    // pointsForm.reset
     document.addEventListener("click", (e) => {
       e.preventDefault();
       let currentUser = qs(".user-stats")
       let id = currentUser.getAttribute("data-id")
+
+      let currentCard = qs(".credit-card")
+      
+      // let ccId = currentCard.id
+      
       if (e.target.matches("#add-points")) {
         let qsPointInput = qs("#accrued-points").value
         let pointsInput = parseInt(qsPointInput)
@@ -136,7 +197,22 @@ document.addEventListener("DOMContentLoaded", () => {
         let totalCash = cashInput + currentCash
         let formData = {accrued_cash: totalCash}
         updateStashValues(id, formData)
+
+      } else if (e.target.matches("#approval-date-edit")) {
+        let qsDateForm = qs("#date-form")
+        let dateInput = qsDateForm.value
+        let ccId = e.target.name
+        let formData = {approval_date: dateInput}
+        updateCard(ccId, formData)
+
+      } else if (e.target.matches("#acquired-bonus")){
+        let qsBonus = qs("#bonus-amt")
+        let bonusInput = qsBonus.value
+        let ccId = e.target.name
+        let formData = {bonus_amount: bonusInput}
+        updateCard(ccId, formData)
       }
+      
     });
   };
 
@@ -163,8 +239,8 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   
   const clickHandler = () => {
-    const navBar = qs(".nav");
-    navBar.addEventListener("click", (e) => {
+    // const navBar = qs(".nav");
+    document.addEventListener("click", (e) => {
       if (e.target.matches(".nav-bar-home")) {
         fetchHome() //add stash values here maybe
       } else if (e.target.matches(".nav-bar-my-cards")) {
@@ -175,18 +251,48 @@ document.addEventListener("DOMContentLoaded", () => {
       } else if (e.target.matches(".nav-bar-browse-cards")) {
         fetchBrowse() //add bookmark functionality -victor
       } else if (e.target.matches(".nav-bar-bookmarks")) {
-        //patch is_bookedmarked true?
+        
+        // patch is_bookedmarked true?
         // if true display here.
       } else if (e.target.matches(".nav-bar-perks")) {
         //same starter code as my cards, but displays global entry and insurance crap
-      } else if (e.target.matches(".edit-self-card"))
-        updateCard(id, formData)
+      } else if (e.target.matches("#bookmark-button")){
+        let button = e.target
+        let cardId = button.previousElementSibling.parentElement.dataset.num
+        // console.log(cardId)
+        // returns id of credit card
+        if (button.innerText === "Bookmark") {
+          //ccId = previosElement.parentElement.dataset.num
+          //dataObject =
+          updateBookmark(ccId, dataObject) 
+          // change button to "Bookmarked!"
+        } else {
+          // patchCard(cardId, "true")
+        }
+        
+      }
       // } else if (e.target.matches(".nav-bar-spend")) {
       // } else if (e.target.matches(".nav-bar-settings")) {
       // } else if (e.target.matches(".nav-bar-notifications")) {
         
     })
   }
+
+  // const patchCard = (cardId, value) => {
+  //   const options = {
+  //     methiod: 'PATCH',
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       "Accept": "application/json"
+  //     },
+  //     body: JSON.stringify({ is_bookedmarked: value })
+  //   }
+  //   fetch((creditCardUrl + cardId), options)
+  //       .then(jsonRes)
+  //       .then((cards) => console.log(cards));
+  //       churnBody.innerText = ""
+    
+  // }
   submitHandler();
   clickHandler();
   getUsers()
